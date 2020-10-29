c9d99703426e79ac8d8527568c9fb2cd564d6252
mgylling <markus.gylling@gmail.com>
2020-10-29 16:51:15 +0100

    add unit test for AssignableDigitalResource

---
+++ src/test/java/org/imsglobal/caliper/v1p2/entities/AssignableDigitalResourceTest.java
@@ -0,0 +1,79 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.imsglobal.caliper.v1p2.entities;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+
+import org.imsglobal.caliper.TestUtils;
+import org.imsglobal.caliper.context.CaliperJsonldContextIRI;
+import org.imsglobal.caliper.context.JsonldStringContext;
+import org.imsglobal.caliper.entities.resource.AssignableDigitalResource;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Category(org.imsglobal.caliper.UnitTest.class)
+public class AssignableDigitalResourceTest {
+	
+    private AssignableDigitalResource entity;
+
+    private static final String BASE_IRI = "https://example.edu";
+    private static final String SECTION_IRI = BASE_IRI.concat("/terms/201601/courses/7/sections/1");
+
+    @Before
+    public void setUp() throws Exception {
+
+        entity = AssignableDigitalResource.builder()
+            .context(JsonldStringContext.create(CaliperJsonldContextIRI.V1P2.value()))
+            .id(SECTION_IRI.concat("/assign/2"))
+            .name("Week 9 Reflection")
+            .storageName("assignment-2.pdf")
+            .description("3-5 page reflection on this week's assigned readings.")                        
+            .dateCreated(new DateTime(2016, 11, 1, 6, 0, 0, 0, DateTimeZone.UTC))
+            .dateToActivate(new DateTime(2016, 11, 10, 11, 59, 59, 0, DateTimeZone.UTC))
+            .dateToShow(new DateTime(2016, 11, 10, 11, 59, 59, 0, DateTimeZone.UTC))
+            .dateToStartOn(new DateTime(2016, 11, 10, 11, 59, 59, 0, DateTimeZone.UTC))
+            .dateToSubmit(new DateTime(2016, 11, 14, 11, 59, 59, 0, DateTimeZone.UTC))
+            .maxAttempts(2)
+            .maxSubmits(2)
+            .maxScore(50.0)            
+            .build();
+    }
+
+    @Test
+    public void caliperEntitySerializesToJSON() throws Exception {
+        ObjectMapper mapper = TestUtils.createCaliperObjectMapper();
+        String json = mapper.writeValueAsString(entity);
+
+        String fixture = jsonFixture("fixtures/v1p2/caliperEntityAssignableDigitalResource.json");
+        JSONAssert.assertEquals(fixture, json, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @After
+    public void teardown() {
+        entity = null;
+    }
+}
