d9287823b28b914328f1efd059db77fe397787d9
mgylling <markus.gylling@gmail.com>
2020-10-30 13:12:21 +0100

    add unit test for SoftwareApplicationUserAgent

---
+++ src/test/java/org/imsglobal/caliper/v1p2/entities/SoftwareApplicationUserAgentTest.java
@@ -0,0 +1,66 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.imsglobal.caliper.v1p2.entities;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+
+import org.imsglobal.caliper.TestUtils;
+import org.imsglobal.caliper.context.CaliperJsonldContextIRI;
+import org.imsglobal.caliper.context.JsonldStringContext;
+import org.imsglobal.caliper.entities.agent.SoftwareApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Category(org.imsglobal.caliper.UnitTest.class)
+public class SoftwareApplicationUserAgentTest {
+	
+    private SoftwareApplication entity;
+
+    @Before
+    public void setUp() throws Exception {
+
+        entity = SoftwareApplication.builder()
+            .context(JsonldStringContext.create(CaliperJsonldContextIRI.V1P2.value()))
+            .id("urn:uuid:d71016dc-ed2f-46f9-ac2c-b93f15f38fdc")
+            .host("https://example.edu")
+            .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36")
+            .ipAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+            .build();
+    }
+
+    @Test
+    public void caliperEntitySerializesToJSON() throws Exception {
+        ObjectMapper mapper = TestUtils.createCaliperObjectMapper();
+        String json = mapper.writeValueAsString(entity);
+
+        String fixture = jsonFixture("fixtures/v1p2/caliperEntitySoftwareApplicationUserAgent.json");
+        JSONAssert.assertEquals(fixture, json, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @After
+    public void teardown() {
+        entity = null;
+    }
+}
